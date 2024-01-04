4d9661394c9080b92f995e6c1d7412f9210cd662
Sławek Zieliński <121642873+SlawekZet@users.noreply.github.com>
2024-01-04 20:34:06 +0100

    Add files via upload

---
+++ functions/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/shorten-url", async (req, res) => {
+  try {
+    const apiUrl = "https://cleanuri.com/api/v1/shorten";
+    const requestData = {
+      url: req.body.url,
+    };
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    });
+
+    const responseData = await response.json();
+
+    res.json({ shortUrl: responseData.result_url });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(3050);
